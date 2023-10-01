83dce4dfe11f2d55f78051a2c2f9f8a79e843890
SuitedIceZ <okshopm575@gmail.com>
2023-10-01 15:32:04 +0700

    [rpi] feat: add json body request template

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import serial
 import re
+import requests
+import json
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
@@ -18,5 +20,20 @@ if __name__ == '__main__':
             output_val1 = float(values[0])
             output_val2 = float(values[1])
             print(output_val1, output_val2)
+            headers =  {"Content-Type":"application/json"}
+            body = {
+                  "nodeGroup": "chair",
+                  "sensor": [
+                    {
+                      "nodeSide": "LEFT",
+                      "weight": output_val1
+                    },
+                    {
+                      "nodeSide": "RIGHT",
+                      "weight": output_val2
+                    }
+                  ]
+                }
+            print("Test body", body)
         else:
             print("Unexpected number of values found!")
