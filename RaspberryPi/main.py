d762145c2726558dc8056ad6492392dd7d9690ba
SuitedIceZ <okshopm575@gmail.com>
2023-10-01 11:45:59 +0700

    [rpi] feat: connect polling arduino via USB

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,1 +1,22 @@
-print("Hello world from rpi python!")
+#!/usr/bin/env python3
+import serial
+import re
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
+    ser.reset_input_buffer()
+
+    pattern = r"output val: ([-+]?[\d]*\.?[\d]+)"
+
+    while True:
+        data = ser.readline()
+        decoded_data = data.decode('utf-8', errors='replace')
+        stripped_data = decoded_data.strip()
+        print(stripped_data)
+        values = re.findall(pattern, stripped_data)
+        if len(values) == 2:
+            output_val1 = float(values[0])
+            output_val2 = float(values[1])
+            print(output_val1, output_val2)
+        else:
+            print("Unexpected number of values found!")
